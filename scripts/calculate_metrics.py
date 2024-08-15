178327d5456e841710994e5704b547de79b336b6
kandrusatish <kandrusatish@gmail.com>
2024-08-15 12:54:11 +0100

    text generation related metrics are now done using evaluate package and also added METEOR metric

---
+++ scripts/calculate_metrics.py
@@ -0,0 +1,30 @@
+# scripts/calculate_metrics.py
+import sys
+import json
+import evaluate
+
+def calculate_metrics(text1, text2):
+    bleu = evaluate.load('bleu')
+    rouge = evaluate.load('rouge')
+    meteor = evaluate.load('meteor')
+
+    bleu_score = bleu.compute(predictions=[text2], references=[text1])
+    rouge_score = rouge.compute(predictions=[text2], references=[text1])
+    meteor_score = meteor.compute(predictions=[text2], references=[text1])
+
+    results = {
+        'bleu': bleu_score,
+        'rouge': rouge_score,
+        'meteor': meteor_score
+    }
+
+    return results
+
+if __name__ == '__main__':
+    input_data = json.loads(sys.stdin.read())
+    text1 = input_data['text1']
+    text2 = input_data['text2']
+
+    metrics = calculate_metrics(text1, text2)
+
+    print(json.dumps(metrics))
